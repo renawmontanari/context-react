55427f7087f0be97144472b98c85b9954a86735b
renawmontanari <regiangelica@gmail.com>
2023-08-11 00:06:16 -0300

    Context

--- context/src/App.js
+++ context/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       
     </div>
-  ); 
+  );  
 }
 
 export default App;
