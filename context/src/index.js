b824385ce22f6e18fcfe0f5b24c769481715d32d
renawmontanari <regiangelica@gmail.com>
2023-08-13 16:25:57 -0300

    Context

--- context/src/index.js
+++ context/src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { CounterContextProvider } from './context/CounterContext';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* Criando provider */}
+  <CounterContextProvider>
     <App />
+  </CounterContextProvider>
   </React.StrictMode>
 );
 
